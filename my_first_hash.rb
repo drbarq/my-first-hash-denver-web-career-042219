9b14d5cc10004daa0055fb66c6bd07f8521f3fe1
Joe Tustin <j.tustin@gmail.com>
2019-03-18 19:57:12 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
-def my_hash(var)
-  my_hash = {"name" => var}
+def my_hash
+  my_hash = {"name" => "Joe"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
 
